445bf862a4670b1796ab6bfda6a7ceb5d1c0122a
javimarashall <jamondragon@ucdavis.edu>
2021-05-13 20:45:08 -0700

    graph works

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,19 +6,17 @@ router.get('/exercise', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/exercise.html'));
   });
 
-//index route   ----------------ROUTE WORKS--------------
-router.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
 
-  // no route is found default to index --------ROUTE WORKS-----------
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+ 
 
 //stats route             ----------ROUTE WORKS------------
-router.get('/dashboard/stats', (req, res) => {
+router.get('/stats', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stats.html'));
   });
 
+ // no route is found default to index --------ROUTE WORKS-----------
+
+  router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 module.exports = router;
